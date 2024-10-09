6146fe91190c67acf622786d8bdf8c7bbd9c92c3
Guilherme Alegria <guilhermemoraes.3@gmail.com>
2024-10-09 15:34:35 +0100

    add cenários

--- cypress/e2e/apitest/customers_request.cy.js
+++ cypress/e2e/apitest/customers_request.cy.js
@@ -1,9 +1,42 @@
 /// <reference types="cypress" />
 
 describe('',() => {
-    context('', () => {
-        it('', () => {
+    context('Request all customers', () => {
+        it('Check return for all customers', () => {
 
         })
     })
+    context('Request all customers with pagination', () => {
+        it('Check pagination with all customers', () => {
+
+        })
+    })
+
+        it('', () => {
+
+        })
+        it('', () => {
+
+        })
+        it('', () => {
+
+        })
+        it('', () => {
+
+        })
+        it('', () => {
+
+        })
+        it('', () => {
+
+        })
+        it('', () => {
+
+        })
+        it('', () => {
+
+        })
+        it('', () => {
+
+        })
 })
