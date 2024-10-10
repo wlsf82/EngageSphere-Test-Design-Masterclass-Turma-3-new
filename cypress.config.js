9dfa187a6b53fd1f12ecc700ca86bc543c03011f
Matheus Barcelos <matheusbarcelos.c@gmail.com>
2024-10-09 23:44:54 -0300

    chore: cypress setup

---
+++ cypress.config.js
@@ -0,0 +1,22 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  env: {
+    API_URL: 'http://localhost:3001',
+  },
+  e2e: {
+    baseUrl: 'http://localhost:3000',
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+  component: {
+    devServer: {
+     framework: 'create-react-app',
+     bundler: 'webpack',
+    },
+    indexHtmlFile: 'frontend/cypress/support/component-index.html',
+    specPattern: 'frontend/src/**/*.cy.{js,jsx,ts,tsx}',
+    supportFile: 'frontend/cypress/support/component.js'
+   },
+});
---
+++ cypress/support/e2e.js
@@ -0,0 +1,3 @@
+import './commands/api_commands'
+import './commands/gui_commands'
+import '@testing-library/cypress/add-commands'
