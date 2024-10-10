445d009f3d0d2b6d513ab9961cf2cc04f9d88267
Matheus Barcelos <matheusbarcelos.c@gmail.com>
2024-10-09 23:50:05 -0300

    feat(tests): create customer empty state tests

--- cypress/e2e/gui/customers.cy.js
+++ cypress/e2e/gui/customers.cy.js
@@ -15,4 +15,24 @@ describe('Customers', () => {
       cy.contains('p', 'Below is our customer list.').should('be.visible');
     });
   });
+
+  describe('Empty State', () => {
+    beforeEach(() => {
+      cy.setCookie('cookieConsent', 'accepted');     
+      cy.visit('/');
+      cy.intercept('GET', `${CUSTOMERS_URL}?page=1&limit=10&size=All&industry=All`, { fixture: 'customers/emptyCustomers' }).as('getEmptyCustomers');
+      cy.get('[data-testid="size-filter"]').select('All');
+      cy.wait('@getEmptyCustomers');
+    });
+
+    it('It shows the image of an empty box and the text "No customers available." when there are no customers in the database', () => {
+      cy.get('svg[title="image of an empty box"]').should('be.visible');
+      cy.contains('span', 'No customers available.').should('be.visible');
+    });
+
+    it('It disables the name text input field when there are no customers in the database', () => {
+      cy.get('#name').should('be.disabled');
+    })
+  });
+
 });
