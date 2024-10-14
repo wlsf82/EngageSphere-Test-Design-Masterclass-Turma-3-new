a7ba04e138d249d036e5d88855c12e1623db1be5
Cicero Henrique <cicero_penha@hotmail.com>
2024-10-14 17:49:28 -0300

    Insert the cookies in the browser

--- cypress/support/commandsGui.js
+++ cypress/support/commandsGui.js
@@ -8,8 +8,9 @@ Cypress.Commands.add('writeMessage', (locator) => {
 Cypress.Commands.add('visitPageAndAcceptCookies', () => {
   const BASE_URL = `${Cypress.env('BASE_URL')}`
   cy.session([], () => {
+    Cypress.on('window:before:load', window => {
+      window.document.cookie = 'cookieConsent=accepted'
+    })
     cy.visit(BASE_URL);
-    cy.contains('button', 'Accept').click()
-    cy.get('.CookieConsent_content__y4iEy').should('not.exist')
   });
 });
