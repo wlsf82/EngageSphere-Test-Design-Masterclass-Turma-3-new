20411cd3f22b04171201c98c5350369bdb8c77dd
Eduardo Effting <eduardo.effting@totvs.com.br>
2024-10-14 18:45:59 -0300

    removendo funcao setupNodeEvents e definindo variavel env para URL no teste de API

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,11 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  env: {
+    API_URL: 'http://localhost:3001',
+  },
   e2e: {
     baseUrl: 'http://localhost:3000',
-    specPattern: 'cypress/e2e/**/*.js',
-    setupNodeEvents(on, config) {
-    },
+    specPattern: 'cypress/e2e/**/*.js'
   },
 });
--- cypress/e2e/API Tests/getCustomers.spec.js
+++ cypress/e2e/API Tests/getCustomers.spec.js
@@ -1,11 +1,15 @@
+const API_URL = Cypress.env('API_URL')
+const CUSTOMERS_API_URL = `${Cypress.env('API_URL')}/customers`
+
+
 describe('API Test - Return customers', () => {
   it('Should return a list of customers', () => {
-    cy.request('GET', 'http://localhost:3001/customers')
-      .then((response) => {
-        expect(response.status).to.eq(200);
-        expect(response.body).to.have.property('customers');
-        expect(response.body.customers).to.be.an('array');
-        expect(response.body.customers.length).to.be.greaterThan(0);
+    cy.request('GET', CUSTOMERS_API_URL)
+      .then(({status, body}) => {
+        expect(status).to.eq(200);
+        expect(body).to.have.property('customers');
+        expect(body.customers).to.be.an('array');
+        expect(body.customers.length).to.be.greaterThan(0);
       });
   });
 });
