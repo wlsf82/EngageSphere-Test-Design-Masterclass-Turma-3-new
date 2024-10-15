acf905e39a58c5fc506cca63f2d5152a524d765d
Guilherme <gdalbo1310@gmail.com>
2024-10-14 21:35:34 -0300

    fixing the API tests

--- cypress.config.js
+++ cypress.config.js
@@ -3,8 +3,9 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   viewportWidth: 1280,
   viewportHeight: 720,
-
-
+  env: {
+    API_URL: 'http://localhost:3001',
+  },
   e2e: {
       baseUrl: 'http://localhost:3000/',
       experimentalRunAllSpecs: true
--- cypress/e2e/api.cy.js
+++ cypress/e2e/api.cy.js
@@ -1,14 +1,12 @@
 /// <reference types = "cypress" />
 
+const API_URL = Cypress.env('API_URL');
 
 describe('API Test Suite', () => {
- 
-    it('should return status 200 for POST request', () => {
-      cy.api({
-      method: 'GET',
-      url: 'http://localhost:3001/customers?page=1&limit=10&size=All&industry=All',
-      }).should((response) => {
-      expect(response.status).to.eq(200);
+  it('should return status 200 for GET request', () => {
+    cy.api('GET','API_URL')
+      .then((status) => {
+        expect(status).to.eq(200)
       });
     });
 });
