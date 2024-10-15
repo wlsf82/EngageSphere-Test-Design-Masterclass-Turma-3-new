3e521a1fd7cef08e72b97e6825ed6812ea0a0b40
Cicero Henrique <cicero_penha@hotmail.com>
2024-10-14 21:44:42 -0300

    Separating the assertions

--- cypress/e2e/gui/testsGui.cy.js
+++ cypress/e2e/gui/testsGui.cy.js
@@ -22,6 +22,7 @@ describe('Validate the GUI', () => {
       cy.contains("Company ID").should('be.visible')
       // Back to companies list
       cy.contains("Back").click()
+
       cy.get('[data-testid="size-filter"]').should('have.value', 'Medium')
       cy.get('[data-testid="industry-filter"]').should('have.value', 'Retail')
       // Check the table lenght
@@ -35,6 +36,7 @@ describe('Validate the GUI', () => {
     cy.contains("Customer Details").should('be.visible')
     // Back to companies list
     cy.contains("Back").click()
+
     cy.contains("Hi there!").should('be.visible')
   })
 
@@ -43,33 +45,38 @@ describe('Validate the GUI', () => {
     cy.contains('a', 'Hotmart')
       .should('be.visible')
       .and('have.attr', 'href', 'https://hotmart.com/pt-br/club/cypress-playground-ate-a-nuvem')
+
     // Check Udemy attributes
     cy.contains('a', 'Udemy')
       .should('be.visible')
       .and('have.attr', 'href', 'https://udemy.com/user/walmyr')
+
     // Check Blog attributes
     cy.contains('a', 'Blog')
       .should('be.visible')
       .and('have.attr', 'href', 'https://talkingabouttesting.com')
+
     // Check Youtube attributes
     cy.contains('a', 'YouTube')
       .should('be.visible')
       .and('have.attr', 'href', 'https://youtube.com/@talkingabouttesting')
+
     // Check Copyright attributes
-    cy.contains('p', 'Copyright 2024 - Talking About Testing').should('be.visible')
+    cy.contains('p', 'Copyright 2024 - Talking About Testing')
+      .should('be.visible')
   })
 
   it('Check welcome message', () => {
     cy.contains("Hi there!").should('be.visible')
-    // Type a name
     cy.get('[data-testid="name"]').type('Cicero Henrique')
+
     cy.contains("Hi Cicero Henrique!").should('be.visible')
   })
 
   it('Header should have title, theme modifier and input', () => {
-    // Check header elements
     cy.get('.Header_container__2fKL4').then(($header) => {
       cy.wrap($header).find('h1').should('have.text', 'EngageSphere');
+
       cy.wrap($header).find('input[type="text"]').should('exist');
       cy.wrap($header).find('button[class="ThemeToggle_button__kDYIH"]').should('exist');
     });
@@ -82,6 +89,8 @@ describe('Validate the GUI', () => {
     // Close messenger
     cy.get('[aria-label="Close messenger"]').should('be.visible')
     cy.get('[aria-label="Close messenger"]').click()
+
+    cy.get('[aria-label="Open messenger"]').should('be.visible')
   })
 
   it('Show and hid alert of sent message', () => {
@@ -94,6 +103,7 @@ describe('Validate the GUI', () => {
     cy.get('.Messenger_sendButton__17Bo4').click()
     cy.contains('Your message has been sent.').should('be.visible')
     cy.tick(10000)
+
     cy.contains('Your message has been sent.').should('not.exist')
   })
 
@@ -107,6 +117,7 @@ describe('Validate the GUI', () => {
     // Open messenger
     cy.get('[aria-label="Open messenger"]').should('be.visible')
     cy.get('[aria-label="Open messenger"]').click()
+
     // Check if the form is empty
     cy.get('#messenger-name').should('have.value', '');
     cy.get('#email').should('have.value', '');
