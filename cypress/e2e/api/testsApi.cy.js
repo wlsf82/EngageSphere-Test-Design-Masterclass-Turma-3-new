4de4752fcfaddcadaee79a8126ed3d73fc873476
Cicero Henrique <cicero_penha@hotmail.com>
2024-10-15 15:53:32 -0300

    Validating the limits of items per page

--- cypress/e2e/api/testsApi.cy.js
+++ cypress/e2e/api/testsApi.cy.js
@@ -10,14 +10,14 @@ describe('Validate the API requests', () => {
   })
 
   it('Change the page and check the first customers', () => {
-    let firstItemId = '';
+    let firstCustomerId = '';
     cy.request({
       method: 'GET',
       url: `${CUSTOMERS_API_URL}?page=1&limit=10&size=All&industry=All`,
     }).then(response => {
 
       expect(response.status).to.equal(200)
-      firstItemId = response.body.customers[0].id;
+      firstCustomerId = response.body.customers[0].id;
     })
 
     cy.request({
@@ -26,7 +26,27 @@ describe('Validate the API requests', () => {
     }).then(response => {
 
       expect(response.status).to.equal(200);
-      expect(response.body.customers[0].id).to.not.equal(firstItemId)
+      expect(response.body.customers[0].id).to.not.equal(firstCustomerId)
+    })
+  })
+
+  it('Change the limits of customers per page', () => {
+    cy.request({
+      method: 'GET',
+      url: `${CUSTOMERS_API_URL}?page=1&limit=10&size=All&industry=All`,
+    }).then(response => {
+
+      expect(response.status).to.equal(200);
+      expect(response.body.pageInfo.totalPages).to.equal(5);
+    })
+
+    cy.request({
+      method: 'GET',
+      url: `${CUSTOMERS_API_URL}?page=1&limit=20&size=All&industry=All`,
+    }).then(response => {
+
+      expect(response.status).to.equal(200);
+      expect(response.body.pageInfo.totalPages).to.equal(3);
     })
   })
 })
