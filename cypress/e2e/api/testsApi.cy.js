bedbdeba90a90f173937411e20bbfcad3cf60da8
Cicero Henrique <cicero_penha@hotmail.com>
2024-10-15 14:16:51 -0300

    Add pagination validation

--- cypress/e2e/api/testsApi.cy.js
+++ cypress/e2e/api/testsApi.cy.js
@@ -3,9 +3,30 @@ describe('Validate the API requests', () => {
   it('Get customers successfully', () => {
     cy.request({
       method: 'GET',
-      url: `${CUSTOMERS_API_URL}?**?page=1&limit=10&size=All&industry=All`,
+      url: `${CUSTOMERS_API_URL}?page=1&limit=10&size=All&industry=All`,
     }).then(response => {
       expect(response.status).to.equal(200)
+    })
+  })
+
+  it('Change the page and check the first customers', () => {
+    let firstItemId = '';
+    cy.request({
+      method: 'GET',
+      url: `${CUSTOMERS_API_URL}?page=1&limit=10&size=All&industry=All`,
+    }).then(response => {
+
+      expect(response.status).to.equal(200)
+      firstItemId = response.body.customers[0].id;
+    })
+
+    cy.request({
+      method: 'GET',
+      url: `${CUSTOMERS_API_URL}?page=2&limit=10&size=All&industry=All`,
+    }).then(response => {
+
+      expect(response.status).to.equal(200);
+      expect(response.body.customers[0].id).to.not.equal(firstItemId)
     })
   })
 })
