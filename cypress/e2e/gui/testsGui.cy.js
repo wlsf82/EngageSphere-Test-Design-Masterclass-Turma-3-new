465f1c2913afec73321de3dcf9c2aa5a8198de7d
Cicero Henrique <cicero_penha@hotmail.com>
2024-10-26 11:11:56 -0300

    Change from many describes to only one

--- cypress/e2e/gui/testsGui.cy.js
+++ cypress/e2e/gui/testsGui.cy.js
@@ -1,208 +1,210 @@
-describe('Validate the no customers scenarios', () => {
-  beforeEach(() => {
-    const CUSTOMERS_API_URL = `${Cypress.env('API_URL')}/customers`
-    cy.intercept(
-      'GET',
-      `${CUSTOMERS_API_URL}?**`,
-      {
-        "customers": [],
-        "pageInfo": {
-          "currentPage": 1,
-          "totalPages": 1,
-          "totalCustomers": 50
-        }
-      }
-    ).as('getNoCustomers');
-    cy.setCookie('cookieConsent', 'accepted')
-    cy.visit('/')
-  })
-
-  it('The empty box image should be visible', () => {
-    cy.get('[title="image of an empty box"]').should('be.visible')
-  })
-
-  it('The input name should be disabled', () => {
-    cy.get('[data-testid="name"]')
-      .should('be.visible')
-      .and('have.attr', 'disabled')
-  })
-})
-
-describe('Validate the customer missing info scenarios', () => {
-  beforeEach(() => {
-    const CUSTOMERS_API_URL = `${Cypress.env('API_URL')}/customers`
-    cy.intercept(
-      'GET',
-      `${CUSTOMERS_API_URL}?**`,
-      { fixture: 'customerMissingInfo.json' }
-    ).as('getCustomer');
-    cy.setCookie('cookieConsent', 'accepted')
-    cy.visit('/')
-    cy.contains('button', 'View').click()
-  })
-
-  it('Should not show contact info', () => {
-    cy.contains('No contact info available').should('be.visible')
-  })
-
-  it('Should not show address', () => {
-    cy.contains('Show address').click()
-    cy.contains('No address available').should('be.visible')
-  })
-})
-
-describe('Validate the cookies', () => {
-  beforeEach(() => {
-    const CUSTOMERS_API_URL = `${Cypress.env('API_URL')}/customers`
-    cy.intercept(
-      'GET',
-      `${CUSTOMERS_API_URL}?**`,
-      { fixture: 'customers' }
-    ).as('getCustomers');
-    cy.visit('/')
-  })
-
-  it('Accept the cookies', () => {
-    cy.contains('button', 'Accept').click()
-    cy.getCookie('cookieConsent').should('have.property', 'value', 'accepted')
-  })
-
-  it('Refuse the cookies', () => {
-    cy.contains('button', 'Decline').click()
-    cy.getCookie('cookieConsent').should('have.property', 'value', 'declined')
-  })
-})
-
 describe('Validate the GUI', () => {
-  beforeEach(() => {
-    const CUSTOMERS_API_URL = `${Cypress.env('API_URL')}/customers`
-    cy.intercept(
-      'GET',
-      `${CUSTOMERS_API_URL}?**`,
-      { fixture: 'customers' }
-    ).as('getCustomers');
-    cy.setCookie('cookieConsent', 'accepted')
-    cy.visit('/')
-  })
-
-  it('Check the load element', () => {
-    cy.get('#loading').should('be.visible')
-  })
-
-  it('Check limit on localstorage', () => {
-    cy.get('[name="pagination-limit"]').select('50')
-    cy.getAllLocalStorage().then((localStorage) => {
-      const { paginationLimit } = localStorage['http://localhost:3000'];
-      expect(paginationLimit).to.equal('50');
-    });
-  })
-
-  it('Show the customer address', () => {
-    cy.contains('button', 'View').click()
-    cy.contains('Show address').click()
-
-    cy.contains('14135 Kari Garden Suite 427').should('be.visible')
-    cy.contains('Mooreshire').should('be.visible')
-    cy.contains('Nevada').should('be.visible')
-    cy.contains('64043').should('be.visible')
-    cy.contains('United States of America').should('be.visible')
-  })
-
-  it('Hide the customer address', () => {
-    cy.contains('button', 'View').click()
-    cy.contains('Show address').click()
-
-    cy.contains('Hide address').click()
-    cy.contains('Show address').should('be.visible')
-  })
-
-  it('Apply a filter, go to a customer page, return to the list page, and check if the previous filters are selected', () => {
-    cy.get('[data-testid="size-filter"]').select('Medium')
-    cy.get('[data-testid="industry-filter"]').select('Retail')
-    cy.wait('@getCustomers')
-    cy.get('[aria-label="View company: Littel Co"]').click()
-    cy.contains("Company ID").should('be.visible')
-    cy.contains("Back").click()
-
-    cy.get('[data-testid="size-filter"]').should('have.value', 'Medium')
-    cy.get('[data-testid="industry-filter"]').should('have.value', 'Retail')
-  })
-
-  it('Move to custmer page and return to clients list', () => {
-    cy.contains('button', 'View').click()
-    cy.contains("Customer Details").should('be.visible')
-    cy.contains("Back").click()
-
-    cy.contains("Hi there!").should('be.visible')
-  })
-
-  it('Check footer texts and links', () => {
-    cy.contains('a', 'Hotmart')
-      .should('be.visible')
-      .and('have.attr', 'href', 'https://hotmart.com/pt-br/club/cypress-playground-ate-a-nuvem')
-
-    cy.contains('a', 'Udemy')
-      .should('be.visible')
-      .and('have.attr', 'href', 'https://udemy.com/user/walmyr')
-
-    cy.contains('a', 'Blog')
-      .should('be.visible')
-      .and('have.attr', 'href', 'https://talkingabouttesting.com')
-
-    cy.contains('a', 'YouTube')
-      .should('be.visible')
-      .and('have.attr', 'href', 'https://youtube.com/@talkingabouttesting')
-
-    cy.contains('p', 'Copyright 2024 - Talking About Testing')
-      .should('be.visible')
-  })
-
-  it('Check welcome message', () => {
-    cy.contains("Hi there!").should('be.visible')
-    cy.get('[data-testid="name"]').type('Cicero Henrique')
-
-    cy.contains("Hi Cicero Henrique!").should('be.visible')
-  })
-
-  it('Header should have title, theme modifier and input', () => {
-    cy.contains('h1', 'EngageSphere').should('be.visible')
-    cy.get('[class^="ThemeToggle_button"]').should('be.visible')
-    cy.get('input[type="text"]').should('be.visible')
-  })
-
-  context('Validate the messenger', () => {
-    it('Open and close the messenger', () => {
-      cy.get('[aria-label="Open messenger"]').should('be.visible').click()
-      cy.get('[aria-label="Close messenger"]').should('be.visible').click()
-
-      cy.get('[aria-label="Open messenger"]').should('be.visible')
+  context('Validate the no customers scenarios', () => {
+    beforeEach(() => {
+      const CUSTOMERS_API_URL = `${Cypress.env('API_URL')}/customers`
+      cy.intercept(
+        'GET',
+        `${CUSTOMERS_API_URL}?**`,
+        {
+          "customers": [],
+          "pageInfo": {
+            "currentPage": 1,
+            "totalPages": 1,
+            "totalCustomers": 50
+          }
+        }
+      ).as('getNoCustomers');
+      cy.setCookie('cookieConsent', 'accepted')
+      cy.visit('/')
     })
 
-    it('Show and hide alert of sent message', () => {
-      cy.clock()
-      cy.get('[aria-label="Open messenger"]').should('be.visible').click()
-      cy.get('#messenger-name').type('test');
-      cy.get('#email').type('test@test.com');
-      cy.get('#message').type('Message test');
-      cy.get('[class^="Messenger_sendButton"]').click()
-      cy.contains('Your message has been sent.').should('be.visible')
-      cy.tick(3000)
-
-      cy.contains('Your message has been sent.').should('not.exist')
+    it('The empty box image should be visible', () => {
+      cy.get('[title="image of an empty box"]').should('be.visible')
     })
 
-    it('Clear and close message form', () => {
-      cy.get('[aria-label="Open messenger"]').should('be.visible').click()
-      cy.get('#messenger-name').type('test');
-      cy.get('#email').type('test@test.com');
-      cy.get('#message').type('Message test');
-      cy.get('[aria-label="Close messenger"]').click()
-      cy.get('[aria-label="Open messenger"]').should('be.visible').click()
+    it('The input name should be disabled', () => {
+      cy.get('[data-testid="name"]')
+        .should('be.visible')
+        .and('have.attr', 'disabled')
+    })
+  })
 
-      // Check if the form is empty
-      cy.get('#messenger-name').should('have.value', '');
-      cy.get('#email').should('have.value', '');
-      cy.get('#message').should('have.value', '');
+  context('Validate the customer missing info scenarios', () => {
+    beforeEach(() => {
+      const CUSTOMERS_API_URL = `${Cypress.env('API_URL')}/customers`
+      cy.intercept(
+        'GET',
+        `${CUSTOMERS_API_URL}?**`,
+        { fixture: 'customerMissingInfo.json' }
+      ).as('getCustomer');
+      cy.setCookie('cookieConsent', 'accepted')
+      cy.visit('/')
+      cy.contains('button', 'View').click()
+    })
+
+    it('Should not show contact info', () => {
+      cy.contains('No contact info available').should('be.visible')
+    })
+
+    it('Should not show address', () => {
+      cy.contains('Show address').click()
+      cy.contains('No address available').should('be.visible')
+    })
+  })
+
+  context('Validate the cookies scenarios', () => {
+    beforeEach(() => {
+      const CUSTOMERS_API_URL = `${Cypress.env('API_URL')}/customers`
+      cy.intercept(
+        'GET',
+        `${CUSTOMERS_API_URL}?**`,
+        { fixture: 'customers' }
+      ).as('getCustomers');
+      cy.visit('/')
+    })
+
+    it('Accept the cookies', () => {
+      cy.contains('button', 'Accept').click()
+      cy.getCookie('cookieConsent').should('have.property', 'value', 'accepted')
+    })
+
+    it('Refuse the cookies', () => {
+      cy.contains('button', 'Decline').click()
+      cy.getCookie('cookieConsent').should('have.property', 'value', 'declined')
+    })
+  })
+
+  context('Validate the customer positive scenarios', () => {
+    beforeEach(() => {
+      const CUSTOMERS_API_URL = `${Cypress.env('API_URL')}/customers`
+      cy.intercept(
+        'GET',
+        `${CUSTOMERS_API_URL}?**`,
+        { fixture: 'customers' }
+      ).as('getCustomers');
+      cy.setCookie('cookieConsent', 'accepted')
+      cy.visit('/')
+    })
+
+    it('Check the load element', () => {
+      cy.get('#loading').should('be.visible')
+    })
+
+    it('Check limit on localstorage', () => {
+      cy.get('[name="pagination-limit"]').select('50')
+      cy.getAllLocalStorage().then((localStorage) => {
+        const { paginationLimit } = localStorage['http://localhost:3000'];
+        expect(paginationLimit).to.equal('50');
+      });
+    })
+
+    it('Show the customer address', () => {
+      cy.contains('button', 'View').click()
+      cy.contains('Show address').click()
+
+      cy.contains('14135 Kari Garden Suite 427').should('be.visible')
+      cy.contains('Mooreshire').should('be.visible')
+      cy.contains('Nevada').should('be.visible')
+      cy.contains('64043').should('be.visible')
+      cy.contains('United States of America').should('be.visible')
+    })
+
+    it('Hide the customer address', () => {
+      cy.contains('button', 'View').click()
+      cy.contains('Show address').click()
+
+      cy.contains('Hide address').click()
+      cy.contains('Show address').should('be.visible')
+    })
+
+    it('Apply a filter, go to a customer page, return to the list page, and check if the previous filters are selected', () => {
+      cy.get('[data-testid="size-filter"]').select('Medium')
+      cy.get('[data-testid="industry-filter"]').select('Retail')
+      cy.wait('@getCustomers')
+      cy.get('[aria-label="View company: Littel Co"]').click()
+      cy.contains("Company ID").should('be.visible')
+      cy.contains("Back").click()
+
+      cy.get('[data-testid="size-filter"]').should('have.value', 'Medium')
+      cy.get('[data-testid="industry-filter"]').should('have.value', 'Retail')
+    })
+
+    it('Move to custmer page and return to clients list', () => {
+      cy.contains('button', 'View').click()
+      cy.contains("Customer Details").should('be.visible')
+      cy.contains("Back").click()
+
+      cy.contains("Hi there!").should('be.visible')
+    })
+
+    it('Check footer texts and links', () => {
+      cy.contains('a', 'Hotmart')
+        .should('be.visible')
+        .and('have.attr', 'href', 'https://hotmart.com/pt-br/club/cypress-playground-ate-a-nuvem')
+
+      cy.contains('a', 'Udemy')
+        .should('be.visible')
+        .and('have.attr', 'href', 'https://udemy.com/user/walmyr')
+
+      cy.contains('a', 'Blog')
+        .should('be.visible')
+        .and('have.attr', 'href', 'https://talkingabouttesting.com')
+
+      cy.contains('a', 'YouTube')
+        .should('be.visible')
+        .and('have.attr', 'href', 'https://youtube.com/@talkingabouttesting')
+
+      cy.contains('p', 'Copyright 2024 - Talking About Testing')
+        .should('be.visible')
+    })
+
+    it('Check welcome message', () => {
+      cy.contains("Hi there!").should('be.visible')
+      cy.get('[data-testid="name"]').type('Cicero Henrique')
+
+      cy.contains("Hi Cicero Henrique!").should('be.visible')
+    })
+
+    it('Header should have title, theme modifier and input', () => {
+      cy.contains('h1', 'EngageSphere').should('be.visible')
+      cy.get('[class^="ThemeToggle_button"]').should('be.visible')
+      cy.get('input[type="text"]').should('be.visible')
+    })
+
+    context('Validate the messenger', () => {
+      it('Open and close the messenger', () => {
+        cy.get('[aria-label="Open messenger"]').should('be.visible').click()
+        cy.get('[aria-label="Close messenger"]').should('be.visible').click()
+
+        cy.get('[aria-label="Open messenger"]').should('be.visible')
+      })
+
+      it('Show and hide alert of sent message', () => {
+        cy.clock()
+        cy.get('[aria-label="Open messenger"]').should('be.visible').click()
+        cy.get('#messenger-name').type('test');
+        cy.get('#email').type('test@test.com');
+        cy.get('#message').type('Message test');
+        cy.get('[class^="Messenger_sendButton"]').click()
+        cy.contains('Your message has been sent.').should('be.visible')
+        cy.tick(3000)
+
+        cy.contains('Your message has been sent.').should('not.exist')
+      })
+
+      it('Clear and close message form', () => {
+        cy.get('[aria-label="Open messenger"]').should('be.visible').click()
+        cy.get('#messenger-name').type('test');
+        cy.get('#email').type('test@test.com');
+        cy.get('#message').type('Message test');
+        cy.get('[aria-label="Close messenger"]').click()
+        cy.get('[aria-label="Open messenger"]').should('be.visible').click()
+
+        // Check if the form is empty
+        cy.get('#messenger-name').should('have.value', '');
+        cy.get('#email').should('have.value', '');
+        cy.get('#message').should('have.value', '');
+      })
     })
   })
 })
