725ccb27d3600d8448991d25621a896b9d6e2434
ahrionn <luam_azf@hotmail.com>
2024-11-01 16:04:12 -0300

    verifica pela visibilidade

--- frontend/cypress/e2e/footer.spec.js
+++ frontend/cypress/e2e/footer.spec.js
@@ -4,28 +4,28 @@ describe('Exibe o rodapé com o texto e links corretos', () => {
   });
 
   it('deve exibir o rodapé com o texto e links corretos', () => {
-    cy.get('[data-testid="footer"]').should('exist');
+    cy.get('[data-testid="footer"]').should('be.visible');
 
     cy.get('[data-testid="footer"]').contains('Copyright 2024 - Talking About Testing');
 
     cy.get('[data-testid="footer"]')
       .find('a[href="https://hotmart.com/pt-br/club/cypress-playground-ate-a-nuvem"]')
-      .should('exist')
+      .should('be.visible')
       .contains('Hotmart');
 
     cy.get('[data-testid="footer"]')
       .find('a[href="https://udemy.com/user/walmyr"]')
-      .should('exist')
+      .should('be.visible')
       .contains('Udemy');
 
     cy.get('[data-testid="footer"]')
       .find('a[href="https://talkingabouttesting.com"]')
-      .should('exist')
+      .should('be.visible')
       .contains('Blog');
 
     cy.get('[data-testid="footer"]')
       .find('a[href="https://youtube.com/@talkingabouttesting"]')
-      .should('exist')
+      .should('be.visible')
       .contains('YouTube');
   });
 });
