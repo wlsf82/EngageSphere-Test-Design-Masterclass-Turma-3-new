f0698f85fc50096b85ffd7a7e269281a5d1b887a
ahrionn <luam_azf@hotmail.com>
2024-11-01 16:57:14 -0300

    melhoria

--- frontend/cypress/e2e/mobileviewport.spec.js
+++ frontend/cypress/e2e/mobileviewport.spec.js
@@ -1,18 +1,18 @@
 describe('Tabela de Clientes - Exibição de colunas em viewport móvel', () => {
-    beforeEach(() => {
-      cy.visit('/');
-  
-      // Simula viewport do Galaxy S20 Ultra
-      cy.viewport(412, 915);
-    });
-  
-    it('Mostra as colunas "Nome da Empresa" e "Ação" e oculta as outras colunas', () => {
-      cy.get('th').contains('Company name').should('be.visible');
-      cy.get('th').contains('Action').should('be.visible');
-  
-      cy.get('th').contains('ID').should('not.be.visible');
-      cy.get('th').contains('Industry').should('not.be.visible');
-      cy.get('th').contains('Number of employees ').should('not.be.visible');
-      cy.get('th').contains('Size').should('not.be.visible');
-    });
-});  
+  beforeEach(() => {
+    cy.visit('/');
+
+    // Simula viewport do Galaxy S20 Ultra
+    cy.viewport(412, 915);
+  });
+
+  it('Mostra as colunas "Nome da Empresa" e "Ação" e oculta as outras colunas', () => {
+    cy.contains('th', 'Company name').should('be.visible');
+    cy.contains('th', 'Action').should('be.visible');
+
+    cy.contains('th', 'ID').should('not.be.visible');
+    cy.contains('th', 'Industry').should('not.be.visible');
+    cy.contains('th', 'Number of employees').should('not.be.visible');
+    cy.contains('th', 'Size').should('not.be.visible');
+  });
+});
