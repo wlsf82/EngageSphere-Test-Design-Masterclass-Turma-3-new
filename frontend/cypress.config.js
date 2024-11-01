48abef3c24d08cb1f0458180603c6527a589aee7
ahrionn <luam_azf@hotmail.com>
2024-11-01 15:38:34 -0300

    correção - .env p/ API & desestruturação de obj response

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -1,8 +1,11 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  env: {
+    API_URL: 'http://localhost:3001',
+  },
   e2e: {
-    baseUrl: 'http://localhost:3001',
+    baseUrl: 'http://localhost:3000',
     specPattern: "cypress/e2e/**/*.spec.js",
   },
 });
--- frontend/cypress/e2e/api.spec.js
+++ frontend/cypress/e2e/api.spec.js
@@ -1,62 +1,63 @@
+const CUSTOMERS_API_URL = `${Cypress.env('API_URL')}/customers`;
+
 describe('API Testing - /customers', () => {
-    const endpoint = '/customers';
   
     it('should return valid customers with default parameters', () => {
-      cy.request(endpoint).then((response) => {
-        expect(response.status).to.eq(200);
-        expect(response.body).to.have.property('customers');
-        expect(response.body.pageInfo).to.have.property('currentPage', 1);
-        expect(response.body.pageInfo).to.have.property('totalPages');
-        expect(response.body.pageInfo).to.have.property('totalCustomers');
+      cy.request(CUSTOMERS_API_URL).then(({ status, body }) => {
+        expect(status).to.eq(200);
+        expect(body).to.have.property('customers');
+        expect(body.pageInfo).to.have.property('currentPage', 1);
+        expect(body.pageInfo).to.have.property('totalPages');
+        expect(body.pageInfo).to.have.property('totalCustomers');
       });
     });
   
     it('should return a 400 error for invalid page or limit', () => {
       cy.request({
         method: 'GET',
-        url: endpoint,
+        url: CUSTOMERS_API_URL,
         qs: { page: -1, limit: 'invalid' },
         failOnStatusCode: false
-      }).then((response) => {
-        expect(response.status).to.eq(400);
-        expect(response.body).to.have.property('error', 'Invalid page or limit. Both must be positive numbers.');
+      }).then(({ status, body }) => {
+        expect(status).to.eq(400);
+        expect(body.error).to.include('Invalid page or limit. Both must be positive numbers.');
       });
     });
   
     it('should return a 400 error for invalid size', () => {
       cy.request({
         method: 'GET',
-        url: endpoint,
+        url: CUSTOMERS_API_URL,
         qs: { size: 'InvalidSize' },
         failOnStatusCode: false
-      }).then((response) => {
-        expect(response.status).to.eq(400);
-        expect(response.body).to.have.property('error', 'Unsupported size value. Supported values are All, Small, Medium, Enterprise, Large Enterprise, and Very Large Enterprise.');
+      }).then(({ status, body }) => {
+        expect(status).to.eq(400);
+        expect(body.error).to.include('Unsupported size value. Supported values are All, Small, Medium, Enterprise, Large Enterprise, and Very Large Enterprise.');
       });
-    });
+    });    
   
     it('should return a 400 error for invalid industry', () => {
       cy.request({
         method: 'GET',
-        url: endpoint,
+        url: CUSTOMERS_API_URL,
         qs: { industry: 'InvalidIndustry' },
         failOnStatusCode: false
-      }).then((response) => {
-        expect(response.status).to.eq(400);
-        expect(response.body).to.have.property('error', 'Unsupported industry value. Supported values are All, Logistics, Retail, Technology, HR, and Finance.');
+      }).then(({ status, body }) => {
+        expect(status).to.eq(400);
+        expect(body).to.include('Unsupported industry value. Supported values are All, Logistics, Retail, Technology, HR, and Finance.');
       });
     });
   
     it('should return filtered customers by size and industry', () => {
       cy.request({
         method: 'GET',
-        url: endpoint,
+        url: CUSTOMERS_API_URL,
         qs: { size: 'Medium', industry: 'Technology' }
-      }).then((response) => {
-        expect(response.status).to.eq(200);
-        expect(response.body.customers).to.be.an('array');
-        expect(response.body.customers.every(c => c.size === 'Medium')).to.be.true;
-        expect(response.body.customers.every(c => c.industry === 'Technology')).to.be.true;
+      }).then(({ status, body }) => {
+        expect(status).to.eq(200);
+        expect(body.customers).to.be.an('array');
+        expect(body.customers.every(c => c.size === 'Medium')).to.be.true;
+        expect(body.customers.every(c => c.industry === 'Technology')).to.be.true;
       });
     });
 });  
