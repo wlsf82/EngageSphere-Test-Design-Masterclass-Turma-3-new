9b50ae90b597d69c2f1ace0550dffb85a3f768d4
ahrionn <luam_azf@hotmail.com>
2024-11-01 14:58:23 -0300

    código não usado deletado

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -4,8 +4,5 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3001',
     specPattern: "cypress/e2e/**/*.spec.js",
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
   },
 });
