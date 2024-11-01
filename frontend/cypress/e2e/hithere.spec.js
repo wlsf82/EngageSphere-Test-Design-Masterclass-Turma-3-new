56a0072cf5f0e971e15c3228298c196b836654a0
ahrionn <luam_azf@hotmail.com>
2024-11-01 16:18:34 -0300

    melhoria hi there

--- frontend/cypress/e2e/hithere.spec.js
+++ frontend/cypress/e2e/hithere.spec.js
@@ -6,6 +6,6 @@ describe('Exibe a saudação "Hi, there!" quando nenhum nome é fornecido', () =
     it('deve exibir "Hi, there!" com a data correta quando nenhum nome é fornecido', () => {
       cy.get('input[data-testid="name"]').clear();
   
-      cy.get('[data-testid="table"] h2').should('contain', 'Hi there!');
+      cy.contains('h2', 'Hi there!').should('be.visible');
     });
 });  
